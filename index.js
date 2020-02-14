7f3733c78d4bae1c9d3e76537df118e9bf2a5986
Lambda_School_Loaner_264 <lambda_school_loaner_264@Lambda-School-Loaner-264s-MacBook-Pro.local>
2020-02-14 12:12:18 -0500

    Added Project Router

--- index.js
+++ index.js
@@ -8,4 +8,10 @@ server.use(express.json())
 server.use('/actions', actionRouter)
 server.use('/projects', projectRouter)
 
+const port = 5000;
+
+server.listen(port, () => {
+    console.log(`Server is up on ${port}`)
+})
+
 module.exports = server
--- router/actionRouter.js
+++ router/actionRouter.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const db = require('../data/helpers/actionModel');
+
 const router = express.Router()
 
 
